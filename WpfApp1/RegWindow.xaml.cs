3247623ebd74843787fa719d74eec62f70c1ab7a
Автамонова Анна Александровна <Avtamonova.A.A@corp.kp11.ru>
2023-10-12 14:06:39 +0300

    first commit

---
+++ WpfApp1/RegWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class RegWindow : Window
+    {
+        public RegWindow()
+        {
+            InitializeComponent();
+            var name = new MyPlaceHolder("Enter your Name here", ref NameTextBox);
+            var email = new MyPlaceHolder("Enter your Email here", ref EmailTextBox);
+            var password = new MyPlaceHolder("Enter your Password here", ref PasswordTextBox);
+            var age = new MyPlaceHolder("18", ref AgeTextBox);
+        }
+
+        private void TextBlock_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            MainWindow MainWindow = new MainWindow();
+            MainWindow.Show();
+            this.Close();
+        }
+
+        private void Image_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+    }
+
+}
